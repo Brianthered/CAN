c2c90af24249a61457fcd81d245c86481214b762
Brian McInnis <bridared@gmail.com>
2023-04-19 16:13:47 -0700

    Playing around with census data

---
+++ Government Data/census data query.sql
@@ -0,0 +1,8 @@
+select * from elephant
+join on elephant.elephant_id = census.elephant_id
+where census.year = 2015
+
+-- Path: Government Data\census data query.sql
+select * from census
+where year = 2015
+
